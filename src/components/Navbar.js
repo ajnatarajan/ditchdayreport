964fc71881599e49e2e4fdf69a36c427a12195e1
ajnatarajan <ajnatarajan@gmail.com>
2022-05-19 21:32:12 -0700

    page structure established

---
+++ src/components/Navbar.js
@@ -0,0 +1,6 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return <div className="nav">Report 'Em All</div>;
+}
+export default Navbar;
---
+++ src/components/ReportSection.js
@@ -0,0 +1,6 @@
+import "./ReportSection.css";
+
+function ReportSection() {
+  return <div className="report-section">Hi!</div>;
+}
+export default ReportSection;
